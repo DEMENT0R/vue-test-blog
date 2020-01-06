2779003ebc5cd647d49b96a1895ffc59b8eb2863
DEMENTOR <dmntr@rambler.ru>
2020-01-06 14:04:21 +1000

    install vuex

--- src/main.js
+++ src/main.js
@@ -1,35 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import VueRouter from 'vue-router'
-Vue.use(VueRouter)
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+Vue.config.productionTip = false;
+
+// Components
 import VueBlog from './components/VueBlog.vue'
 import VuePosts from './components/VuePosts.vue'
 import VueUsers from './components/VueUsers.vue'
 import VueComments from './components/VueComments.vue'
 
-// 1.
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-// 2.
 const routes = [
-  { path: '/', component: VueBlog },
-  { path: '/users', component: VueUsers },
-  { path: '/posts', component: VuePosts },
-  { path: '/comments', component: VueComments },
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar },
-  { path: '/test', component: VueBlog }
-]
-// 3.
+    {path: '/', component: VueBlog},
+    {path: '/users', component: VueUsers},
+    {path: '/posts', component: VuePosts},
+    {path: '/comments', component: VueComments}
+];
 const router = new VueRouter({
-  routes
-})
+    routes
+});
 
 new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
+    render: h => h(App),
+    router
+}).$mount('#app');
